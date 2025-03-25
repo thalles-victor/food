e09ed437b24bae1e5286e8038ecf45a4e3d8e88e
thalles-victor <thalles.victor2012@gmail.com>
2025-03-25 02:33:03 -0300

    :wrench: conf: the server.

--- food-backend/src/main.ts
+++ food-backend/src/main.ts
@@ -1,8 +1,18 @@
+import 'dotenv/config';
+
+import 'src/Application/@shared/env';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+  app.enableVersioning();
+
+  const PORT = process.env.PORT ?? 3000;
+  await app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
 }
 bootstrap();
