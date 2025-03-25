176127c03ded88595d99b6a730cc389aa071c660
thalles-victor <thalles.victor2012@gmail.com>
2025-03-25 04:50:48 -0300

    :sparkles: feat: implement auth controller.

--- food-backend/src/Application/Domains/Auth/Auth.controller.ts
+++ food-backend/src/Application/Domains/Auth/Auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './Auth.service';
+import { SignUpDto } from './dtos/SignUp.dto';
+import { SignInDto } from './dtos/SignIn.dto';
 
 @Controller({ path: 'auth', version: '1' })
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signUp')
+  async signUp(@Body() userDto: SignUpDto) {
+    return this.authService.signUp(userDto);
+  }
+
+  @Post('signIn')
+  async signIn(@Body() authDto: SignInDto) {
+    return this.authService.sinIn(authDto);
+  }
+}
